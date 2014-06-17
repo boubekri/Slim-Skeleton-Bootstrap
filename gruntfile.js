8f652c6f898aac6446ed0fea95c2a3ae50131935
Jake Whetstone <jake@indrez.com>
2014-06-17 13:29:45 -0400

    Setup bootstrap and jquery via components. Gruntfile setup.

---
+++ gruntfile.js
@@ -0,0 +1,59 @@
+'use strict';
+
+module.exports = function (grunt) {
+    // load all grunt tasks
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.initConfig({
+        watch: {
+            // if any .less file changes in directory "public/css/" run the "less"-task.
+            files: ["less/app.less","less/bootstrap/*.less"],
+            tasks: ["less:dev"]
+        },
+        // "less"-task configuration
+        less: {
+            dev: {
+                options: {
+                    paths: ["less/"]
+                },
+                files: {
+                    "public/css/app.css": "less/app.less"
+                }
+            },
+            dist:{
+                options: {
+                    paths: ["less/"],
+                    cleancss: true
+                },
+                files: {
+                    "public/css/app.css": "less/app.less"
+                }
+            }
+        },
+        copy: {
+            less: {
+                cwd: 'components/bootstrap/less',
+                src: '**/*',
+                dest: 'less/bootstrap',
+                expand: true
+            },
+            js: {
+                cwd: 'components/bootstrap/js',
+                src: 'bootstrap.min.js',
+                dest: 'public/js/vendor',
+                expand: true
+            },
+            fonts: {
+                cwd: 'components/bootstrap/fonts',
+                src: '**/*',
+                dest: 'public/fonts',
+                expand: true
+            }
+
+        }
+    });
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('dist', ['less:dist']);
+};
