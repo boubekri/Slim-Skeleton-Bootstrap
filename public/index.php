2a3bb461fa97b7510c94b50166d7beb6a5668465
Jake Whetstone <jake@indrez.com>
2014-06-17 14:00:24 -0400

    Bootstrap base template

--- public/index.php
+++ public/index.php
@@ -30,7 +30,7 @@ $app->get('/', function () use ($app) {
     // Sample log message
     $app->log->info("Slim-Skeleton '/' route");
     // Render index view
-    $app->render('index.html');
+    $app->render('index.html.twig');
 });
 
 // Run app
